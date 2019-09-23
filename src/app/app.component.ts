bbd5e981fb5b1c07864acfd379efa53516a45d33
Felipe Vieira Baehr <felipebaehr@gmail.com>
2019-09-23 16:57:11 -0300

    asdfjh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UtilsService } from 'projects/ottimizza/utils/src/public-api';
+import { DateUtils } from 'projects/ottimizza/utils/src/lib/date.util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ottimizza';
+
+  constructor(private dateUtil: DateUtils) {
+    console.log(this.dateUtil.dateToString())
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UtilsModule } from 'projects/ottimizza/utils/src/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UtilsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
